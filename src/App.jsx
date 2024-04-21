1b65c282394fb4289594f8e1ab1cc6f128c677aa
carlosfermed <harmonium_skies@hotmail.com>
2024-04-21 19:07:44 +0200

    Rectificada estructura de carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Formulario } from './Formulario'
+import { Formulario } from './components/Formulario'
 
 const App = () => {
 
