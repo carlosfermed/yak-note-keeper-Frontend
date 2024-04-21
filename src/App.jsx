74103e31abe06c32d69ff3d65be0f27c7ca4c677
carlosfermed <harmonium_skies@hotmail.com>
2024-04-21 18:12:21 +0200

    Versión con Fetch

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { Formulario } from './Formulario'
+
+const App = () => {
+
+  const [notesArray, setNotesArray] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    fetch("http://localhost:3001/notes")
+      .then(response => response.json())
+      .then(data => setNotesArray(data))
+      .catch(err => console.log(err))    
+  }
+
+  return (
+    <>
+      <fieldset>
+        <legend>Manipulación de notas</legend>
+        <Formulario notesArray={notesArray}/>
+        <br/>
+        <button onClick={fetchData}>Actualizar notas (Fetch)</button>
+      </fieldset>
+      <ul>
+        {notesArray.map(note => <li key={note.id}>{note.content}</li>)}
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ src/Formulario.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+const Formulario = () => {
+
+  const [note, setNote] = useState({
+    id: "", 
+    content: "", 
+    important: false
+  })
+
+  const sendData = () => {
+    
+    fetch("http://localhost:3001/notes", {
+      method: "POST", 
+      body: JSON.stringify(note),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(note => { console.log(note) })
+      .catch(err => console.log(err))
+  }
+
+  const addNote = (e) => {
+    console.log('e.target.name :>> ', e.target.name);
+    console.log('e.target.value :>> ', e.target.value);
+    setNote({
+      ...note,
+      id: Date.now(),
+      [e.target.name]: e.target.value      
+    })
+  }
+
+  return (
+    <>
+      <form onSubmit={addNote}>
+        Contenido de la nota: <input type='text' name='content' value={note.content} onChange={addNote}/>
+        <br/>
+        <span className='tituloRadio'>Es importante: </span>
+        SI <input type='radio' name='important' value='true' onChange={addNote} />
+        NO <input type='radio' name='important' value='false' onChange={addNote} />
+        <br/><br/>
+        <button onClick={sendData}>Enviar datos (POST)</button>
+      </form>
+    </>
+  )
+}
+
+export {Formulario}
